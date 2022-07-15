8c91e928c4799e2600e1dd5b5a74f5d2ed9b1301
unknown <denuzzidouglas@gmail.com>
2022-07-15 14:45:12 -0300

    respawn_working

--- main.py
+++ main.py
@@ -13,12 +13,24 @@ print("|##################################################|")
 
 print("\nO programa logo iniciará, minimize esta tela antes dele prosseguir...")
 
-time.sleep(7)
+time.sleep(2)
 
-def click(x, y):
-    win32api.SetCursor(x, y)
+def Cursor():
+    win32api.SetCursorPos((10,10))
+
+def click(ps_x, ps_y):
+    win32api.SetCursorPos((ps_x, ps_y))
+    time.sleep(0.1)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
+    time.sleep(0.1)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
+
+def menu_open():
+    pyautogui.press('esc')
+    time.sleep(0.3)
+    pyautogui.press('enter')
+    time.sleep(0.3)
+    pyautogui.press('right')
 
 
 print("\nPrograma iniciado!")
@@ -27,6 +39,28 @@ Combate = False
 Barragem = False
 Ouro = False
 Testes = 0
+cxy = False
+
+
+### LOCALIZAR POSIÇÃO DO SINO DE MONSTRO
+
+menu_open()
+
+while cxy == False:
+    if pyautogui.locateOnScreen('Images-PTBR\\SinoBorder.png', confidence=0.90) != None:
+        print('Achei')
+        blx, bly = pyautogui.locateCenterOnScreen('Images-PTBR\\SinoBorder.png', confidence=0.85)
+        cxy = True
+    else:
+        pyautogui.press('down')
+        print('Ainda localizando...')
+
+print(blx)
+print(bly)
+
+# Fechar o menu
+pyautogui.press('esc')
+pyautogui.press('esc')
 
 ### FARMAR MANTIS
 
@@ -35,7 +69,7 @@ while keyboard.is_pressed("q") == False:
         Mantis = False
         start = time.time()
         while time.time() - start < 2:
-            if pyautogui.locateOnScreen('Images-PTBR\\Mantis_Body.png', confidence=0.7) != None:
+            if pyautogui.locateOnScreen('Images-PTBR\\Mantis_Body.png' or 'Images-PTBR\\Mantis_Body_Alt.png' , confidence=0.7) != None:
                 Mantis = True
                 if Mantis == True:
                     start = 2
@@ -51,40 +85,29 @@ while keyboard.is_pressed("q") == False:
             pyautogui.keyUp("right")
             print("Combate Iniciado!")
             Combate = True
-        else:
-#            print("Mantis não localizada!")
-            contador_2 = 0
-            pyautogui.press('esc') # Abre o menu
+        else: # Utiliza um sino de monstro.
+            print("Pegando sino!")
+            menu_open()
+            print(blx)
+            print(bly)
+            click(blx, bly)
             time.sleep(0.2)
-            pyautogui.press('enter') # Abre Inventário
-            time.sleep(0.2)
-            pyautogui.press('right') # Vai para Consumíveis
-            time.sleep(0.2)
-            btn_sin = pyautogui.locateOnScreen('Images-PTBR\\Sino_de_Monstro.png', confidence=0.7)
-            btn_sin_e = pyautogui.center(btn_sin)
-            pyautogui.click(btn_sin_e.x, btn_sin_e.y)
-            print("Sino de monstro alcançado.")
-            # Tem um problema, se estiver na outra página o sino, ele dá dois enters e quebra tudo.
-            while contador_2 != 2:
-                pyautogui.press('enter')
-                time.sleep(0.2)
-                contador_2 += 1
+            pyautogui.press('enter')
 
     if pyautogui.locateOnScreen('Images-PTBR\\Iniciar_Combate.png', confidence=0.7) != None:
             pyautogui.press('enter')
-            time.sleep(3)
+            time.sleep(0.5)
             Barragem = True
 
     while Barragem == True:
-        if pyautogui.locateOnScreen('Images-PTBR\\Barragem_de_Gemas.png', confidence=0.7) != None:
-                    btn = pyautogui.locateOnScreen('Images-PTBR\\Barragem_de_Gemas.png', confidence=0.7)
-                    btn_e = pyautogui.center(btn)
+        if pyautogui.locateOnScreen('Images-PTBR\\Barragem_de_Gemas.png', confidence=0.95) != None:
+                    btnx, btny = pyautogui.locateCenterOnScreen('Images-PTBR\\Barragem_de_Gemas.png', confidence=0.95)
                     time.sleep(0.3)
                     print("Clicado na Barragem de Gemas")
-                    pyautogui.click(btn_e.x, btn_e.y)
+                    click(btnx, btny)
                     pyautogui.press('enter')
                     pyautogui.press('enter')
-                    time.sleep(4)
+                    time.sleep(3.8)
                     pyautogui.press('enter')
                     Barragem = False
                     print("Combate Feito!")
@@ -105,3 +128,5 @@ while keyboard.is_pressed("q") == False:
                     print(Testes)
         else:
                 print("Ainda localizando...")
+
+print("Não há mais sinos restantes ou o programa falhou.")
